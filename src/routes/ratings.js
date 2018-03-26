5f5c5dada9e802f79713a55f008f60e347dae123
rcvink <rrvink@gmail.com>
2018-03-26 16:39:01 +0100

    Overhaul rating confirmation page

--- src/routes/ratings.js
+++ src/routes/ratings.js
@@ -2,17 +2,23 @@ var express = require('express');
 var Rating = require('../models/rating');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('ratings');
-});
-
 router.post('/', function(req, res) {
   var rating = new Rating({
     rating: req.body.rating,
     created_at: new Date()
   });
   rating.save();
-  res.redirect('/ratings');
+  res.redirect(`/ratings/${rating._id}`);
+});
+
+router.get('/:ratingId', function(req, res) {
+  Rating.findOne({ _id: req.params.ratingId }, function(err, rating) {
+    if (err) {
+      res.render('error', { error: err });
+    } else {
+      res.render('confirmation', { rating: rating.rating, date: rating.created_at });
+    };
+  });
 });
 
 module.exports = router;
--- test/rating-confirmed-page.js
+++ test/rating-confirmed-page.js
@@ -2,14 +2,28 @@ const Browser = require('zombie');
 
 describe('Confirmation page', function() {
   const browser = new Browser();
+  var mockDate;
 
-  before(function(done) {
-    browser.visit('/ratings', done);
+  beforeEach(function(done) {
+    browser.visit('/', done);
+  });
+
+  beforeEach(function(done) {
+    browser.pressButton('button[id=very-happy]', done);
+    mockDate = new Date();
+  })
+
+  it('loads successfully', function() {
+    browser.assert.success();
   });
 
   it('thanks the user for their feedback', function() {
     browser.assert.text('h1', 'Thanks for your feedback.');
   });
+
+  it('states the rating\'s score and date', function() {
+    browser.assert.text('p', `Your rating of 3 out of 3 was stored on ${mockDate}`);
+  })
 
   it('has a back button', function() {
     browser.assert.element('button[id=back]');
